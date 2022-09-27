6b9fc7383c32b606187fe9264af97dc6d0ec8133
Elite <89701094+Aarush06@users.noreply.github.com>
2022-09-26 19:49:15 -0500

    done

--- main.js
+++ main.js
@@ -1,43 +1,21 @@
-// Triangle Perimeter Assignment Start Code
+// Triangle Perimeter Assignment By Aarush
+document.getElementById('button').addEventListener('click', btnClicked);
 
+function btnClicked() {
+    let xA = +document.getElementById("x1").value;
+    let xB = +document.getElementById("x2").value;
+    let xC = +document.getElementById("x3").value;
+    let yA = +document.getElementById("y1").value;
+    let yB = +document.getElementById("y2").value;
+    let yC = +document.getElementById("y3").value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function dist() {
-
+    //Calculate and Output Area and Perimeter
+    document.getElementById("ab").innerHTML = dist(xA, yA, yA, yB);
+    document.getElementById("ac").innerHTML = dist(xA, yA, xC, yC);
+    document.getElementById("bc").innerHTML = dist(xB, yB, xC, yC);
+    document.getElementById("perimeter").innerHTML = dist(xA, yA, xB, yB) + dist(xA, yA, xC, yC) + dist(xB, yB, xC, yC);
 }
+function dist(x1, x2, y1, y2) {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+}
+// Side note: Im proud of myself for doing it in 20 lines I couldnt find anyone who did it in less 🥱
